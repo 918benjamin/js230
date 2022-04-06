901d87c8ccdd5b3d73a12a208ad110ce8ce5714d
Benjamin Perrault <benjamin.perrault@gmail.com>
2022-04-06 17:59:34 -0500

    Add promise exercises

---
+++ lesson_2/promises.js
@@ -0,0 +1,24 @@
+// let myPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('Launch School');
+//   }, 2000);
+// })
+
+// myPromise.then((resolution) => {
+//   console.log(resolution);
+// })
+
+
+
+// let myPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject('Error: Not Launch School');
+//   }, 2000)
+// });
+
+// myPromise.catch(value => {
+//   console.log(value);
+// })
+
+
+
