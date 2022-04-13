bdd880aa7f0f020fcb13af31d8d908d46b04a5f0
Benjamin Perrault <benjamin.perrault@gmail.com>
2022-04-13 12:53:54 -0500

    refactor removing the highlight with LS solution that doesn't involve walking the DOM

--- exercises/events_async/highlighter.js
+++ exercises/events_async/highlighter.js
@@ -1,26 +1,16 @@
-function walk(node, callback) {
-  callback(node);
-
-  for (let index = 0; index < node.childNodes.length; index += 1) {
-    walk(node.childNodes[index], callback);
+function removeHighlight() {
+  const highlighted = document.querySelector('.highlight');
+  if (highlighted) {
+    highlighted.classList.remove('highlight');
   }
 }
-
-function clearClassFromAllNodes(className) {
-  walk(document.body, node => {
-    if (node.nodeType === 1 && node.classList.contains(className)) {
-      node.classList.remove(className);
-    }
-  });
-}
-
 
 document.addEventListener('DOMContentLoaded', () => {
   document.body.addEventListener('click', event => {
     event.stopPropagation();
     let target = event.target;
 
-    clearClassFromAllNodes('highlight');
+    removeHighlight();
     if (target.tagName === 'A') {
       document.querySelector(target.hash).classList.add('highlight');
     } else if (target.closest('article')) {
