5b4b226e0a57a9c9b4e174ded55a4b141d85a516
Benjamin Perrault <benjamin.perrault@gmail.com>
2022-04-25 16:10:23 -0500

    Add refactored add staff page

--- exercises/http/booking_app_node/public/javascripts/add_staff.js
+++ exercises/http/booking_app_node/public/javascripts/add_staff.js
@@ -4,24 +4,23 @@
   // if not, alert staff can't be created
 document.addEventListener('DOMContentLoaded', () => {
   let form = document.querySelector('form');
+
   form.addEventListener('submit', event => {
     event.preventDefault();
     let formData = new FormData(form);
-    let email = formData.get('email');
-    let name = formData.get('name');
-
-    if (!email || !name) {
-      alert('Staff can not be created. Check your inputs');
-      return;
-    }
 
     let request = new XMLHttpRequest();
     request.open('POST', '/api/staff_members');
-    request.responseType = 'json';
 
-    request.addEventListener('load', event => {
-      let id = request.response.id;
-      alert(`Successfully created staff with id: ${id}`);
+    request.addEventListener('load', () => {
+      if (request.status === 400) {
+        alert(request.response);
+      } else {
+        let data = JSON.parse(request.response);
+        let id = data.id;
+        alert(`Successfully created staff with id: ${id}`);
+        form.reset();
+      }
     });
 
     request.addEventListener('error', event => {
