3b2dcb191cd1f54111ae59e79c604ec4c0a7bf81
Benjamin Perrault <benjamin.perrault@gmail.com>
2022-06-09 10:23:29 -0500

    Add part 1 requirements

--- lesson_5/photo_gallery2/public/javascripts/gallery.js
+++ lesson_5/photo_gallery2/public/javascripts/gallery.js
@@ -5,7 +5,7 @@
     // render these comments using the handlebars each loop
       // pass in the photos object and comments object
       // specific photo needs to be passed in
-// New comment creates a post request to /comments/new
+// New comment creates a POST request to /comments/new
   // Then load that comment
 
 // On page load, ajax call to get comments for first photo
@@ -24,7 +24,49 @@
 document.addEventListener('DOMContentLoaded', () => {
   // ajax request for JSON data of all photos
   // then, render photos template
-    // write rendered template to #slides
+    // write rendered template to #slides div
   // render photo_information template using first photo data
     // write rendered template to section > header
+  const templates = {};
+  let photos;
+
+  document.querySelectorAll('script[type="text/x-handlebars"]').forEach(tmpl => {
+    templates[tmpl.id] = Handlebars.compile(tmpl.innerHTML);
+  });
+
+  document.querySelectorAll("[data-type=partial]").forEach(partial => {
+    Handlebars.registerPartial(partial.id, partial.innerHTML);
+  });
+
+  fetch("/photos")
+    .then(response => response.json())
+    .then(json => {
+      photos = json;
+      renderPhotos();
+      renderPhotoInformation(photos[0].id);
+      getCommentsFor(photos[0].id);
+    });
+  
+  function renderPhotos() {
+    let slidesDiv = document.getElementById('slides');
+    slidesDiv.innerHTML = templates.photos({photos: photos});
+  }
+
+  function renderPhotoInformation(id) {
+    let photo = photos.filter(photoData => {
+      return photoData.id === id;
+    })[0];
+
+    let header = document.querySelector('section > header');
+    header.innerHTML = templates.photo_information(photo);
+  }
+
+  function getCommentsFor(index) {
+    fetch(`/comments?photo_id=${index}`)
+      .then(response => response.json())
+      .then(json => {
+        let ul = document.querySelector('#comments > ul');
+        ul.innerHTML = templates.photo_comments({comments: json});
+      });
+  }
 });
